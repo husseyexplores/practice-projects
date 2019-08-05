be1b6d8e8f08bdf0e22a4dfeeb35120c17a2750d
husseyexplores <husseyexplores@gmail.com>
2019-08-05 18:28:43 +0500

    Remove existing styles and style nav with tailwind

--- node-basic-online-store/public/js/main.js
+++ node-basic-online-store/public/js/main.js
@@ -1,16 +1,21 @@
-const backdrop = document.querySelector('.backdrop')
-const sideDrawer = document.querySelector('.mobile-nav')
-const menuToggle = document.querySelector('#side-menu-toggle')
+(() => {
+  const $toggleNav = $('#toggle-nav')
+  const $navHamburger = $('#nav-hamburger')
+  const $navCross = $('#nav-cross')
+  const $nav = $('#nav')
+  let isNavOpen = false
 
-function backdropClickHandler() {
-  backdrop.style.display = 'none'
-  sideDrawer.classList.remove('open')
-}
-
-function menuToggleClickHandler() {
-  backdrop.style.display = 'block'
-  sideDrawer.classList.add('open')
-}
-
-backdrop.addEventListener('click', backdropClickHandler)
-menuToggle.addEventListener('click', menuToggleClickHandler)
+  $toggleNav.click(e => {
+    e.preventDefault()
+    isNavOpen = !isNavOpen
+    if (isNavOpen) {
+      $nav.removeClass('hidden')
+      $navHamburger.hide()
+      $navCross.show()
+    } else {
+      $nav.addClass('hidden')
+      $navHamburger.show()
+      $navCross.hide()
+    }
+  })
+})()
