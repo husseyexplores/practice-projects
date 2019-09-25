172ccaa0ba825f76b9e6bafd08c7b5f41346b78f
husseyexplores <husseyexplores@gmail.com>
2019-09-24 22:45:31 -0700

    Prisma scaffolding & setup graphql with yoga

--- react-graphql-online-store/backend/src/resolvers/Mutation.js
+++ react-graphql-online-store/backend/src/resolvers/Mutation.js
@@ -1,3 +1,18 @@
-const mutations = {}
+const handleize = str =>
+  str
+    .toLowerCase()
+    .replace(/[^\w\u00C0-\u024f]+/g, '-')
+    .replace(/^-+|-+$/g, '')
 
-module.exports = mutations
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if the user is authenticated
+
+    const data = { ...args.data, handle: handleize(args.data.title) }
+    const item = await ctx.db.mutation.createItem({ data }, info)
+
+    return item
+  },
+}
+
+module.exports = Mutations
