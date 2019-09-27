effade3ec84306a85e1cb0bd1929e0725ad9258b
husseyexplores <husseyexplores@gmail.com>
2019-09-27 07:00:53 -0700

    Refactor items page to use Home page

--- react-graphql-online-store/frontend/pages/items.js
+++ react-graphql-online-store/frontend/pages/items.js
@@ -1,15 +1,1 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Home from './index'
-
-// ----------------------------------------------------------------------------
-
-function items() {
-  return <Home />
-}
-
-items.propTypes = {}
-
-items.defaultProps = {}
-
-export default items
+export { default } from './index'
